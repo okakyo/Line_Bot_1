9791b24253be30ffd4b7ff394eccd1b7102fad91
okakyo <0622okakyo@gmail.com>
2020-05-26 23:21:43 +0900

    設定中

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,28 @@
-import * as functions from 'firebase-functions';
+import { NestFactory } from '@nestjs/core'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import * as express from 'express'
+import * as functions from 'firebase-functions'
+import * as helmet from 'helmet'
+import { AppModule } from './app.module'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const server = express()
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance)
+  )
+
+  // ここにセキュリティについての設定を追加する
+  app.use(helmet())
+  app.enableCors()
+
+  console.log('the server is starting @ firebase')
+  return app.init()
+}
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err))
+
+export const api = functions.https.onRequest(server)
